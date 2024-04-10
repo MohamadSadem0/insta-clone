052e2600972bc71263a5b657670fb8b95f6f3c07
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-04-10 21:39:13 +0300

    .

--- frontend/src/pages/Authentication/components/LoginForm.jsx
+++ frontend/src/pages/Authentication/components/LoginForm.jsx
@@ -70,39 +70,3 @@ const LoginForm = ({ onSubmit, toggleForm, error }) => {
 };
 
 export default LoginForm;
-
-//  <div className="signup-container flex row wrap">
-//       <Section width={"width-50"} styling={"min-width-500"}>
-//         <div className="content flex">
-//           <div className="signup-block">
-//             <p className="landing-title">
-//               Join the biggest professional community
-//             </p>
-//             <div className="signup-inputs-container flex column">
-//               <Input type={"auth-input border"} placeholder={"Email"} />
-//               <Input
-//                 type={"auth-input border"}
-//                 placeholder={"Password"}
-//                 input_type={"password"}
-//               />
-
-//               <Button type={"auth-button dark-bg white-txt"} text={"SIGN IN"} />
-//             </div>
-
-//             <div className="auth-switch-container flex row">
-//               <p>Already have an account?</p>
-//               <Button
-//                 type={"auth-switch-button blue-txt white-bg"}
-//                 text={"Sign In"}
-//                 onClick={() => setSignup(!signup)}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </Section>
-//       <Section width={"width-50"} styling={"white-bg"}>
-//         <div className="content">
-//           <img className="hero-image" src={image} alt={image} />
-//         </div>
-//       </Section>
-//     </div>
--- frontend/src/pages/Authentication/components/SignupForm.jsx
+++ frontend/src/pages/Authentication/components/SignupForm.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 const SignupForm = ({ onSubmit, toggleForm, error }) => {
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
   const handleChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
@@ -14,44 +18,65 @@ const SignupForm = ({ onSubmit, toggleForm, error }) => {
 
   return (
     <div className="flex justify-center h-auto my-10">
-      <div className="border-solid border-2 w-80 flex flex-col py-10 px-10">
-        {error && <p className="error">{error}</p>}
+      <div className="max-w-md w-full mx-auto px-6 py-10 border-solid border-2 border-gray-300 rounded-md">
+        <img
+          src={require("../../../assets/images.png")}
+          className="h-11 mt-5 ml-auto mr-auto mb-10"
+          alt=""
+        />
+        <div className="text-center">
+          <h2 className="text-xl font-semibold mb-5">
+            {" "}
+            Sign Up to see photos and videos
+            <div>from your friends</div>
+          </h2>
+          {error && <p className="text-red-500 mb-5">{error}</p>}
+        </div>
         <form onSubmit={handleSubmit}>
-          <div className="form-group">
+          <div className="mb-4">
             <input
               type="email"
               name="email"
               value={credentials.email}
               onChange={handleChange}
-              className="border-solid border-2 h-9 mb-2 rounded w-auto py-2 px-2"
+              className="border border-gray-300 rounded-md px-3 py-2 w-full"
               placeholder="Email"
               required
             />
           </div>
-          <div className="form-group">
+          <div className="mb-4">
             <input
               type="password"
               name="password"
               value={credentials.password}
               onChange={handleChange}
-              className="border-solid border-2 h-9 py-2 px-2"
+              className="border border-gray-300 rounded-md px-3 py-2 w-full"
               placeholder="Password"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              name="username"
+              value={credentials.username}
+              onChange={handleChange}
+              className="border border-gray-300 rounded-md px-3 py-2 w-full"
+              placeholder="Username"
               required
             />
           </div>
           <button
             type="submit"
-            className="bg-blue-400 rounded-md text-white mt-5 py-1"
+            className="bg-blue-400 hover:bg-blue-500 text-white rounded-md py-2 px-4 w-full"
           >
             Sign Up
           </button>
         </form>
-        <div className="pt-8">
+        <div className="mt-8 text-center">
           Already have an account?{" "}
-          <span>
-            <a className="text-blue-400" onClick={toggleForm} href="#">
-              Log in
-            </a>
+          <span className="text-blue-400 cursor-pointer" onClick={toggleForm}>
+            Log in
           </span>
         </div>
       </div>
