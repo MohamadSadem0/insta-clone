b89b08c8f19bc05e4a307403ff2e9e59ed017c27
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-04-10 10:50:59 +0300

    .

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { useEffect,useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Authentication/index"
+
+const App=()=> {
+  useEffect(( )=>{},[])
+  return (
+    <div className="App">
+<BrowserRouter>
+<Routes>
+  <Route path='/auth' element={<Auth/>}></Route>
+</Routes>
+</BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
